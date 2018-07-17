45f743e641a6aa5f79c36c0a83bc1dae440e2e25
Daniel <15012375+danmastrow@users.noreply.github.com>
2018-07-17 12:12:15 +1000

    Reorganised project and solution, changed DEBUG build output path. Created basic GUI for installing and managing the service.

---
+++ OutlookAppointmentSchedulerGUI/MainForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace OutlookAppointmentSchedulerGUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceController1 = new System.ServiceProcess.ServiceController();
+            this.buttonLaunch = new System.Windows.Forms.Button();
+            this.buttonInstall = new System.Windows.Forms.Button();
+            this.buttonUninstall = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonLaunch
+            // 
+            this.buttonLaunch.Location = new System.Drawing.Point(330, 240);
+            this.buttonLaunch.Name = "buttonLaunch";
+            this.buttonLaunch.Size = new System.Drawing.Size(103, 23);
+            this.buttonLaunch.TabIndex = 0;
+            this.buttonLaunch.Text = "Launch";
+            this.buttonLaunch.UseVisualStyleBackColor = true;
+            this.buttonLaunch.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonInstall
+            // 
+            this.buttonInstall.Location = new System.Drawing.Point(330, 269);
+            this.buttonInstall.Name = "buttonInstall";
+            this.buttonInstall.Size = new System.Drawing.Size(103, 23);
+            this.buttonInstall.TabIndex = 1;
+            this.buttonInstall.Text = "Install Service";
+            this.buttonInstall.UseVisualStyleBackColor = true;
+            this.buttonInstall.Click += new System.EventHandler(this.buttonInstall_Click);
+            // 
+            // buttonUninstall
+            // 
+            this.buttonUninstall.Location = new System.Drawing.Point(330, 298);
+            this.buttonUninstall.Name = "buttonUninstall";
+            this.buttonUninstall.Size = new System.Drawing.Size(103, 23);
+            this.buttonUninstall.TabIndex = 2;
+            this.buttonUninstall.Text = "Uninstall Service";
+            this.buttonUninstall.UseVisualStyleBackColor = true;
+            this.buttonUninstall.Click += new System.EventHandler(this.buttonUninstall_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 240);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(103, 23);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "Start Service";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(12, 269);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(103, 23);
+            this.buttonStop.TabIndex = 4;
+            this.buttonStop.Text = "Stop Service";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 353);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonUninstall);
+            this.Controls.Add(this.buttonInstall);
+            this.Controls.Add(this.buttonLaunch);
+            this.Name = "MainForm";
+            this.Text = "Outlook Appointment Scheduler GUI";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceController serviceController1;
+        private System.Windows.Forms.Button buttonLaunch;
+        private System.Windows.Forms.Button buttonInstall;
+        private System.Windows.Forms.Button buttonUninstall;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+    }
+}
+
---
+++ OutlookAppointmentSchedulerGUI/MainForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OutlookAppointmentSchedulerGUI
+{
+    public partial class MainForm : Form
+    {
+        private readonly string serviceName = "OutlookAppointmentScheduler";
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            serviceController1.ServiceName = serviceName;
+            serviceController1.MachineName = Environment.MachineName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string file = Path.Combine
+            var process = Process.Start("OutlookAppointmentScheduler.exe");
+            //serviceController1.Start();
+        }
+
+        private void buttonInstall_Click(object sender, EventArgs e)
+        {
+            var process = Process.Start("OutlookAppointmentScheduler.exe", "install --autostart");
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            serviceController1.Start();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            serviceController1.Stop();
+        }
+
+        private void buttonUninstall_Click(object sender, EventArgs e)
+        {
+            var process = Process.Start("OutlookAppointmentScheduler.exe", "uninstall");
+        }
+    }
+}
