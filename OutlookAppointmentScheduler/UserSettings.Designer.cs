f4319db7b16d9566b0e908f3bdc1218006b932ab
Daniel <15012375+danmastrow@users.noreply.github.com>
2018-07-24 14:11:34 +1000

    Updated the default start type of the OAS Service to TodayAt

--- OutlookAppointmentScheduler/UserSettings.Designer.cs
+++ OutlookAppointmentScheduler/UserSettings.Designer.cs
@@ -12,7 +12,7 @@ namespace OutlookAppointmentScheduler {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
     internal sealed partial class UserSettings : global::System.Configuration.ApplicationSettingsBase {
         
         private static UserSettings defaultInstance = ((UserSettings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new UserSettings())));
@@ -61,7 +61,7 @@ namespace OutlookAppointmentScheduler {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("StartNow")]
+        [global::System.Configuration.DefaultSettingValueAttribute("TodayAt")]
         public string StartType {
             get {
                 return ((string)(this["StartType"]));
